2a070356aef476a376d1b73131d2e60121af5dfa
Dmitry Melekh <melekh.dm@gmail.com>
2021-11-25 21:12:47 +0300

    d33, news preview sorted

--- projects/NewsPortal/Portal/news/views.py
+++ projects/NewsPortal/Portal/news/views.py
@@ -8,16 +8,16 @@ from news.models import Post
 class NewsList(ListView):
     # model = Post
     # указываем модель, объекты которой мы будем выводить
-    template_name = 'news.html'
+    template_name = 'news_preview.html'
     # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'news'
     # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
     def get_queryset(self):
-        return Post.objects.filter(type=Post.news)
+        return Post.objects.filter(type=Post.news).order_by('-time_in')
 
 class NewsDetail(DetailView):
     # model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
-    template_name = 'news.html'  # название шаблона будет product.html
-    context_object_name = 'news'  # название объекта. в нём будет
+    template_name = 'article.html'  # название шаблона будет product.html
+    context_object_name = 'article'  # название объекта. в нём будет
     def get_queryset(self):
         return Post.objects.filter(type=Post.news)
